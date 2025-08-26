885e982a6433c297de7dcee56a10737dcbb2d814
guibato <149600470+guibato@users.noreply.github.com>
2025-08-26 17:50:19 -0300

    cgpt7

--- Imobiliaria/sisimob/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class SisimobConfig(AppConfig):
-    name = 'sisimob'
-
-    def ready(self):
-        # Importa os sinais do core
-        import Imobiliaria.core.signals  # noqa: F401
